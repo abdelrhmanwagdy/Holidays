43ca43fa5995bf023109226530366d61a4358fb6
abdelrahman <abdelrahman@tonse.co>
2021-12-06 16:59:42 +0200

    adding country class (Model)

---
+++ CountryProject/Models/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryProject.Models
+{
+    public class Country
+    {
+        public int id { set; get; }
+        public string name { set; get; }
+        public string code { set; get; }
+    }
+}
