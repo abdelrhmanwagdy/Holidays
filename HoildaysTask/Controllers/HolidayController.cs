6d3eb20d87e91deff4cd041aee76c6f9468209d8
abdelrahman <abdelrahman@tonse.co>
2021-12-06 21:19:52 +0200

    adding crud operations in controller except post method

---
+++ HoildaysTask/Controllers/HolidayController.cs
@@ -0,0 +1,135 @@
+﻿using CountryProject.Controllers;
+using CountryProject.Models;
+using HoildaysProject.Data;
+using HoildaysProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HoildaysProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class HolidayController : ControllerBase
+    {
+        private readonly CountryController _countryController;
+        private readonly TaskDbContext _context;
+        private readonly IHolidayMethods _holidayMethods;
+
+
+        public HolidayController(TaskDbContext _context, IHolidayMethods _holidayMethods, CountryController _countryController)
+        {
+            this._holidayMethods = _holidayMethods;
+            this._context = _context;
+            this._countryController = _countryController;
+        }
+
+        //for server
+        public Country getCountryByName(string country)
+        {
+            return _countryController.GetCountryByName(country);
+        }
+
+        //for server
+        public IEnumerable<Holiday> GetAllHolidaysForServer()
+        {
+            return _holidayMethods.GetAllHolidaysForServer();
+        }
+
+        [HttpDelete("{holidayId}")]
+        public IActionResult GetHolidayById(string id)
+        {
+            try {
+                Holiday holiday = _holidayMethods.GetHoliday(id);
+                if (holiday == null)
+                {
+                    Response.StatusCode = StatusCodes.Status404NotFound;
+                    return new JsonResult(new { Error = "No Holidays with the specified id exists" });
+                }
+                else
+                {
+                    return new JsonResult(new { Holiday = holiday });
+                }
+            }
+            catch (Exception ex) 
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return new JsonResult(new { Error = ex.Message });
+            }
+
+        }
+
+
+        [HttpDelete("{countryName}/{holidayId}")]
+        public IActionResult DeleteHolidaysForSpecificCountryByCountryName(string countryName, string id)
+        {
+            try
+            {
+                _holidayMethods.DeleteHolidaysForSpecificCountryByCountryName(countryName, id);
+                return new JsonResult(new { Result = "Deleted" });
+            }
+            catch (ArgumentException ex)
+            {
+                Response.StatusCode = StatusCodes.Status404NotFound;
+                return new JsonResult(new { Error = ex });
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return new JsonResult(new { Error = ex.Message });
+            }
+        }
+
+        [HttpDelete("{holidayId}")]
+        public IActionResult DeleteHolidayById(string id)
+        {
+            try
+            {
+                _holidayMethods.DeleteHoliday(id);
+                return new JsonResult(new { Result = "Deleted" });
+            }
+            catch (ArgumentException ex)
+            {
+                Response.StatusCode = StatusCodes.Status404NotFound;
+                return new JsonResult(new { Error = ex });
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return new JsonResult(new { Error = ex.Message });
+            }
+        }
+
+
+        [HttpPut("{holidayId}")]
+        public IActionResult UpdateHoliday(string holidayId, [FromBody] Holiday body)
+        {
+            try
+            {
+                Holiday holiday = _holidayMethods.UpdateHoliday(holidayId, body);
+                return new JsonResult(new { UpdatedHoliday = holiday });
+            }
+            catch (ArgumentException ex)
+            {
+                Response.StatusCode = StatusCodes.Status404NotFound;
+                return new JsonResult(new { Error = ex });
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return new JsonResult(new { Error = ex.Message });
+            }
+        }
+
+
+
+
+    }
+}
--- HoildaysTask/Models/IHolidayMethods.cs
+++ HoildaysTask/Models/IHolidayMethods.cs
@@ -5,14 +5,13 @@ using System.Threading.Tasks;
 
 namespace HoildaysProject.Models
 {
-    interface IHolidayMethods
+    public interface IHolidayMethods
     {
         public IEnumerable<Holiday> GetHolidaysForClients(int _skip);
         public Holiday GetHoliday(string id);
         public void DeleteHolidaysForSpecificCountryByCountryName(string countryName, string id);
         public Holiday UpdateHoliday(string id, Holiday body);
         public void DeleteHoliday(string id);
-
         public IEnumerable<Holiday> GetAllHolidaysForServer();
     }
 }
