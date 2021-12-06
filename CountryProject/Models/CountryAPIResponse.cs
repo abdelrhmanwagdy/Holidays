27edfe61f16fc329e0ade4c74a95aff95d4bf46c
abdelrahman <abdelrahman@tonse.co>
2021-12-06 17:12:09 +0200

    adding CountryAPIResponse class (model)

---
+++ CountryProject/Models/CountryAPIResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryProject.Models
+{
+    public class CountryAPIResponse
+    {
+        public CountryName name { set; get; }
+        public string cca2 { get; set; }
+    }
+
+    public class CountryName
+    {
+        public string common { get; set; }
+    }
+
+}
