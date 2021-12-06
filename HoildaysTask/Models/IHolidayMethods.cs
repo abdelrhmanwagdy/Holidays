6db8035f1c0006f4fe952d61cf8749d32abe945e
abdelrahman <abdelrahman@tonse.co>
2021-12-06 20:26:26 +0200

    adding IHoliday interface and implement it using MySQLHolidayMethods.cs which interact with mysql database

---
+++ HoildaysTask/Models/IHolidayMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HoildaysProject.Models
+{
+    interface IHolidayMethods
+    {
+        public IEnumerable<Holiday> GetHolidaysForClients(int _skip);
+        public IEnumerable<Holiday> GetHolidaysForSpecificCountryByCountryId(int countryId);
+        public IEnumerable<Holiday> GetHolidaysForSpecificCountryByCountryName(string countryName);
+        public Holiday PostHolidaysForSpecificCountryByCountryName(string countryName, Holiday body);
+        public void DeleteHolidaysForSpecificCountryByCountryName(string countryName, string id);
+        public Holiday UpdateHoliday(string id, Holiday body);
+
+        public IEnumerable<Holiday> GetAllHolidaysForServer();
+    }
+}
---
+++ HoildaysTask/Models/MySQLHolidayMethods.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HoildaysProject.Models
+{
+    public class MySQLHolidayMethods : IHolidayMethods
+    {
+        public IEnumerable<Holiday> GetAllHolidaysForServer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Holiday> GetHolidaysForClients(int _skip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Holiday> GetHolidaysForSpecificCountryByCountryId(int countryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Holiday> GetHolidaysForSpecificCountryByCountryName(string countryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Holiday PostHolidaysForSpecificCountryByCountryName(string countryName, Holiday body)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Holiday UpdateHoliday(string id, Holiday body)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteHolidaysForSpecificCountryByCountryName(string countryName, string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
